ef897aa74acb9f91a6e07822668c1a1d2f78381a
Trishala-K1 <121046665+Trishala-K1@users.noreply.github.com>
2022-12-29 14:06:58 -0500

    Update 4.Dictionaries.py

--- 4.Dictionaries.py
+++ 4.Dictionaries.py
@@ -28,4 +28,25 @@ KeyNumber = FavNum['Key']
 print(f"Soj's favourite number is {SojNumber}.")
 print(f"Key's favourite number is also {KeyNumber}.")
 
+#Looping through key-value pairs in a dictionary using items() method.
+for key, value in FavNum.items():
+    print(f"\nKey: {key}")
+    print(f"\nValue: {value}")
+    print(f"{key}'s favorite number is {value}.")
+print("* * *")
+#Looping through all keys in a dictionary using key() method.
+FavNum = {'Soj' : '1', 'Key' : '7'}
+Friends = {'Soj', 'Key', 'Trish'}
+for name in sorted(FavNum.keys()):
+    print(f"Hi, {name}!")
+    if name in Friends:
+        Number = FavNum[name]
+        print(f"\t{name}, I see your favorte number is {Number}!")
+if 'Trish' not in FavNum.keys():
+    print("Trish, please tell us your favorite number!")
 
+#Looping through all values in a dictionary using values() method.
+FavNum = {'Soj' : '1', 'Key' : '7'}
+print("The following numbers have been mentioned: ")
+for numbers in FavNum.values():
+    print(numbers)
